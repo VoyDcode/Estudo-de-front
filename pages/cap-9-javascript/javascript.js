25cdb4963229c8c7448d98c70a6ad3ac26ad4478
VoyDcode <Victor_r.lima@hotmail.com>
2026-04-21 19:16:54 -0300

    Ajustes de ancoragem e adição de estudos de javascript

--- pages/cap-9-javascript/javascript.js
+++ pages/cap-9-javascript/javascript.js
@@ -3,6 +3,47 @@
     Comentário de múltiplas linhas
 */
 
+
+// Declarando variaveis
+
+// let permite que a variável seja reatribuída posteriormente
+let nome = "Victor";
+let idade = 26;
+let altura = 1.84;
+let isStudent = true;
+
+// const significa que a variável é constante e não pode ser reatribuída depois de definida
+const email = "victor_r.lima@hotmail.com";
+const data_nascimento = "21/10/1999";
+const cpf = "321.123.456-78";
+const rg = "23.365.924-6";
+
+//declarando as variáveis
+let userName = 'Clark Kent';
+let userId = 12345;
+let userLogged = true;
+let userEmail;
+
+// alert("Olá, " + nome + "! boa noite!");
+// let mensagem = prompt("Digite uma mensagem para exibir no console:");
+
+// console.log(`mensagem digitada: ${mensagem}`);
+
+// confirm("Deseja continuar?");
+
+
 // usando console
-console.log("Olá, mundo!");
-console.log("Victor" );
+
+console.log("Olá, " + nome + "! Você tem " + idade + " anos e " + altura + "m de altura.");
+console.log("Seu email é " + email);
+console.log("Sua data de nascimento é " + data_nascimento);
+console.log("Seu CPF é " + cpf);
+console.log("Seu RG é " + rg);
+
+console.log("------------------------------");
+console.log(" ");
+
+console.log("id:" + userId, typeof userId);
+console.log( "Nome usuário: " + userName, typeof userName);
+console.log("Está logado: " +  userLogged,  typeof userLogged); 
+console.log("Email do usuário: " + userEmail, typeof userEmail);
---
+++ pages/cap-9-javascript/operadoresMatematicos.js
@@ -0,0 +1,31 @@
+let primeiroNumero = 50;
+let segundoNumero = 10;
+
+console.log(`Primeiro numero: ${primeiroNumero}, Tipo: ${typeof primeiroNumero}`);
+console.log(`Segundo numero: ${segundoNumero}, Tipo: ${typeof segundoNumero}`);
+
+// Adição
+
+console.log(`Soma: ${primeiroNumero + segundoNumero}`);
+// ou
+soma = primeiroNumero + segundoNumero;
+console.log("soma: " + soma);
+// mais estetico
+console.log(`${primeiroNumero} + ${segundoNumero} = ${soma}`);
+
+// Subtração
+
+console.log(`Subtração: ${primeiroNumero - segundoNumero}`);
+
+// Multiplicação   
+console.log(`Multiplicação: ${primeiroNumero * segundoNumero}`);
+
+// Divisão
+console.log(`Divisão: ${primeiroNumero / segundoNumero}`);      
+
+// Porcentagem
+console.log(`Porcentagem: ${primeiroNumero * segundoNumero / 100}`);
+
+
+
+
