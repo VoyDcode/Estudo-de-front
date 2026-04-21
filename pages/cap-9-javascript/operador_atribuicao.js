d90053fdffad96049366a981fa87a5e573f3e81b
VoyDcode <Victor_r.lima@hotmail.com>
2026-04-21 19:55:20 -0300

    Alinha do template de testes para arquivos js e estudos sobre incremento, drecremento e atribuição.

---
+++ pages/cap-9-javascript/operador_atribuicao.js
@@ -0,0 +1,17 @@
+let primeiroNumero = 100;
+console.log(`Primeiro numero: ${primeiroNumero}`);
+
+primeiroNumero+= 20;
+console.log(`Atrubiocao de soma +=20 em 100: ${primeiroNumero}`);
+
+primeiroNumero-= 50;
+console.log(`Atrubiocao de subtracao -=50 em 120: ${primeiroNumero}`);
+
+primeiroNumero*= 2;
+console.log(`Atrubiocao de multiplicacao *=2 em 70: ${primeiroNumero}`);    
+
+primeiroNumero/= 2;
+console.log(`Atrubiocao de divisao /=2 em 140: ${primeiroNumero}`);
+
+primeiroNumero%= 3;
+console.log(`Atrubiocao de modulo %=3 em 70: ${primeiroNumero}`);   
---
+++ pages/cap-9-javascript/operador_incremento.js
@@ -0,0 +1,9 @@
+let primeiroNumero = 30;
+let segundoNumero = 50;
+console.log(`Primeiro numero: ${primeiroNumero}, Segundo numero: ${segundoNumero}`);
+
+primeiroNumero++;
+console.log(`Primeiro numero(incrementado): ${primeiroNumero}`);
+
+segundoNumero--;
+console.log(`Segundo numero(decrementado): ${segundoNumero}`);
