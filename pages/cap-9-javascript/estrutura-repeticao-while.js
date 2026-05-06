d57ca2b6e30e0055b6dbfa1ffe51976074a67f90
VoyDcode <Victor_r.lima@hotmail.com>
2026-05-06 00:09:47 -0300

    Ajuste de cor da pagina de testes e estudo da estrutura while

---
+++ pages/cap-9-javascript/estrutura-repeticao-while.js
@@ -0,0 +1,6 @@
+// pedir ao usuario a digitação do numero 7
+let numero = Number(prompt("Digite o numero 7"));
+    while(numero !== 7){
+        numero = Number(prompt("Erro... digite o numero 7"));
+    }
+console.log(numero);
---
+++ pages/cap-9-javascript/funcoes.js
@@ -0,0 +1,16 @@
+function soma(){
+    let primeiroNumero = 20;
+    let segundoNumero = 30;
+    console.log(primeiroNumero + segundoNumero);
+}
+soma();
+console.log("-------------------------------------------------------")
+
+function multiplicacao(v1, v2){
+    let resultado = v1 * v2;
+    console.log(`O resultado da funcão é: ${resultado}`);
+}
+
+
+multiplicacao(2, 10);
+console.log("-------------------------------------------------------")
