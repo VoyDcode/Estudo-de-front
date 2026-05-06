30403338138591ad2c744283e2add0cf87b509e7
VoyDcode <Victor_r.lima@hotmail.com>
2026-05-06 02:13:59 -0300

    Funções com logica de descontos.

--- pages/cap-9-javascript/funcoes.js
+++ pages/cap-9-javascript/funcoes.js
@@ -11,6 +11,31 @@ function multiplicacao(v1, v2){
     console.log(`O resultado da funcão é: ${resultado}`);
 }
 
-
 multiplicacao(2, 10);
 console.log("-------------------------------------------------------")
+
+function subtrair (valor1, valor2){
+    return valor1 - valor2;
+}
+
+const resultadoFuncao = subtrair(8,4);
+console.log(resultadoFuncao);
+console.log("-------------------------------------------------------")
+
+function porcentagemDeAlgo(valor1, valor2){
+    return valor1 * valor2 / 100;
+}
+
+function porcentagemValorDesconto(valor1){
+     return valor1 * 0.90; // Desconto de 10% já incluido (preço final, oque o cliente paga)
+}
+
+function porcentagemComDesconto(valor1){
+    return valor1 * 0.10; //  Retorna quantos reais de desconto o cliente teve(que foi dado)
+}
+
+function calcularPrecoFinal(preco, desconto) {
+    // Se o desconto for 10, (100 - 10) / 100 vira 0.90
+    return preco * ((100 - desconto) / 100);
+}
+console.log(calcularPrecoFinal(200, 15)); // 15% de desconto em 200
